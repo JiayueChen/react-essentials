955f1d82b170961ff46b7b6c063b72a9c9de79ce
Julia Chen <chenjy527@gmail.com>
2022-04-03 17:56:47 -0400

    check state

--- src/App.js
+++ src/App.js
@@ -1,57 +1,27 @@
 import React from "react";
 import "./App.css";
-import restaurant from "./restaurant.jpg"
+import restaurant from "./restaurant.jpg";
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  );
+function SecreteComponent() {
+  return <h1>Secret informaton for authorized users only</h1>
+}
+function Regularcomponent() {
+  return <h1>Everyone can see this comment</h1>
 }
 
-function Main(props) {
+function App(props) {
+  // check props.authorized value
   return (
-    <section>
-      <p>We serve the most {props.adjective} food around.</p>
-      {/* adding image */}
-      <img src={restaurant} height={200} alt="napkin and table" />
-      {/* use JS camelcase for css style  */}
-      <ul style={{textAlign: "left"}}>
-        {props.dishes.map((dish)=> (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  );
-}
+    <>
+      {props.authorized ? <SecreteComponent/> : <Regularcomponent />  }
+    </>
+  )
 
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-}
-
-const dishes = [
-  "macaroni and cheese",
-  "salmon",
-  "tofu",
-  "minestrone"
-];
-
-const dishObjects = dishes.map((dish, i) => ({id: i, title: dish}))
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Cindy"/>
-      {/* pass dishobject with key as id */}
-      <Main adjective="amazing" dishes={dishObjects}/>
-      <Footer year={new Date().getFullYear()}/>
-    </div>
-  );
+  // if(props.authorized) {
+  //   return <SecreteComponent/>;
+  // } else {
+  //   return <Regularcomponent />;
+  // }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,27 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-function AppTwo() {
-    return <h1>this is the second app</h1>;
-}
 ReactDOM.render(
-    // we can't render two sibling component unless they're wrapped in another tag like <div>
-    // <div>
-    //     <App />
-    //     <AppTwo/>
-    // </div>,
-
-    // <React.Fragment></React.Fragment> or <></> can be used to wrap content without adding extra div
-    // <React.Fragment>
-    //     <App />
-    //     <AppTwo/>
-    // </React.Fragment>,
-
-    // <>
-    //     <App />
-    //     <AppTwo/>
-    // </>,
-
-    <App />,
+    <App authorized={false} />,
     document.getElementById("root")
 );
